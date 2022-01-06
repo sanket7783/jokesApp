368d5f7f5f5eb69f5e29eadc42cfe3606b1245a6
sanket.hambir <sanket.hambir@pubmatic.com>
2022-01-06 16:47:53 +0530

    Jokes App: Initial commit.

---
+++ src/test/java/spring/jokesApp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.jokesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
